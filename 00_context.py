f1a5d0f38bc2d1b693116369acebe475a4b34dfc
ramona <ramona@mikucare.com>
2024-05-07 11:33:19 -0400

    Updates to DB Comyrights

--- 00_context.py
+++ 00_context.py
@@ -11,7 +11,7 @@
 # MAGIC
 # MAGIC Wind power forecasting is essential for adequate power system stability, dispatching and cost control. Wind power is a favorable power source for green electricity generation and prediction is a crucial topic in reducing the energy lifecycle's unpredictability. Balancing energy supply and demand is critical to ensure that all energy produced from wind power is in deed actually being used. The goal of the Solution Accelerator is to provide dataset-based prediction tool for estimating wind power. Additionally, graph and tablular visulizations are provided to relay a better understanding of the wind and power production relationship and the algoristhms and methods used for forecasted predictions.
 #
-# Using public SACADA datasets that include theoretical power, produced active power, wind direction, wind speed, month and hour, generated turbine power is forecasted using machine learning algorithms.
+# Using public SCADA datasets that include theoretical power, produced active power, wind direction, wind speed, month and hour, generated turbine power is forecasted using machine learning algorithms.
 # MAGIC
 # MAGIC ** Authors**
 # MAGIC - Ramona Niederhausern  [<rnieder@newmathdata.com>]
