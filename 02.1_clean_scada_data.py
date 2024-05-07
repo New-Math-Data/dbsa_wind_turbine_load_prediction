1adc7058d503596ccb7d1d2c436b1b8cea004d32
Ramona Niederhausern <rnieder@newmathdata.com>
2024-05-07 15:21:23 -0400

    setting files so db recognizes them as Notebooks

--- 02.1_clean_scada_data.py
+++ 02.1_clean_scada_data.py
@@ -6,8 +6,3 @@
 # MAGIC   2. ......
 
 # COMMAND ----------
-
-
-# TODO: use service table to determine if the turbine was under maintenance on any given day and time
-# TODO: if maintenance delete from fit
-# TODO: make sure turbine make and model is of the same generator capacity
