c2d378b0980f02962f99440ec232165da887b3c7
ramonajenny <ramonajenny>
2024-05-08 18:06:58 +0000

    testing
    testing repo git commands

--- 01.1_ingest_and_create_tables_for_scada_data.py
+++ 01.1_ingest_and_create_tables_for_scada_data.py
@@ -9,7 +9,7 @@
 # MAGIC
 # MAGIC Wind power forecasting is essential for adequate power system stability, dispatching and cost control. Wind power is a favorable power source for green electricity generation and prediction is a crucial topic in reducing the energy lifecycle's unpredictability. Balancing energy supply and demand is critical to ensure that all energy produced from wind power is in deed actually being used. The goal of the Solution Accelerator is to provide dataset-based prediction tool for estimating wind power. Additionally, graph and tablular visulizations are provided to relay a better understanding of the wind and power production relationship and the algoristhms and methods used for forecasted predictions.
 # MAGIC
-# Using public SCADA datasets that include theoretical power, produced active power, wind direction, wind speed, month and hour, generated turbine power is forecasted using machine learning algorithms.
+# MAGIC # Using public SCADA datasets that include theoretical power, produced active power, wind direction, wind speed, month and hour, generated turbine power is forecasted using machine learning algorithms.
 # MAGIC
 # MAGIC ** Authors**
 # MAGIC - Ona Niederhausern  [<rnieder@newmathdata.com>]
@@ -38,6 +38,7 @@
 # MAGIC
 # MAGIC In this step, we will:
 # MAGIC   1. Setup notebook configuration
+
 # MAGIC %run ./config/notebook_config
 
 # COMMAND ----------
@@ -62,9 +63,9 @@ unzip -o /Workspace/Repos/rnieder@newmathdata.com/dbsa_wind_turbine_load_predict
 
 # COMMAND ----------
 
-spark.sql(f"DROP DATABASE IF EXISTS {wind_turbine_load_prediction} CASCADE")
-spark.sql(f"CREATE DATABASE {wind_turbine_load_prediction}")
-spark.sql(f"USE {wind_turbine_load_prediction}")
+spark.sql(f"DROP DATABASE IF EXISTS {sa_wind_turbine_load_prediction} CASCADE")
+spark.sql(f"CREATE DATABASE {sa_wind_turbine_load_prediction}")
+spark.sql(f"USE {sa_wind_turbine_load_prediction}")
 
 # COMMAND ----------
 
@@ -93,4 +94,5 @@ SELECT * FROM scada_data LIMIT 10
 
 # MAGIC
 # COMMAND ----------
+
 
