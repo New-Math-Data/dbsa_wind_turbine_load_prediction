12b0c5f9d016f4a3b18b63f7098f73ae878b9e72
ramonajenny <ramonajenny>
2024-05-20 20:44:30 +0000

    removed Databrick Secrets utility -- could not get it to work -- permissions problem maybe
    removed Databrick Secrets utility -- could not get it to work -- permissions problem maybe

--- 08_weather_data_ingest.py
+++ 08_weather_data_ingest.py
@@ -7,21 +7,18 @@
 # MAGIC %md
 # MAGIC ### Overview - Ingest Streamed Pipeline Data
 # MAGIC
+# MAGIC * Create a free user account with https://www.visualcrossing.com/ to object an api key and gain access to their api. 
+# MAGIC
+# MAGIC * Note: There is an option to instead run the forcasted weather example dataset in `datasets/example_forecasted_weather_data.json`
+# MAGIC
 # MAGIC * Connect VisualCrossing API Endpoint to retrieve 15 days of forcasted weather data in Yalova, Marmara Bölgesi, Turkey. Note: The weather data obtained from the VisualCrossing API is forecasted on an hourly basis, whereas our SCADA data was computed using 10-minute intervals.
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ##### Let's adhere to best practices by securely storing our API Key using the Databricks Secrets utility
-
-# COMMAND ----------
-
-# MAGIC %sh databricks secrets put --scope forecasted_weather_data --key --"5MKDL5XX9XK4RGDWC4B5MJQXE"
-
-# COMMAND ----------
-
-# Read secret from the secret scope
-secret_value = dbutils.secrets.get(scope="forecasted_weather_data", key="key")
+# MAGIC ##### Create a free user account with https://www.visualcrossing.com/ to object an api key and gain access to their api.
+# MAGIC
+# MAGIC ###### Note: There is an option to instead run the forcasted weather example dataset in datasets/example_forecasted_weather_data.json
 
 # COMMAND ----------
 
@@ -34,15 +31,13 @@ import urllib.request
 import sys
 import json
 
-# Ingest forecasted weather data                
+# Ingest forecasted weather data 
 try:
   # Forcasted 15 days of weather data
-  # weather_results_bytes = urllib.request.urlopen("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/yalova%2C%20turkey?unitGroup=metric&include=days%2Chours&key=5MKDL5XX9XK4RGDWC4B5MJQXE&contentType=json")
-
-  weather_results_bytes = urllib.request.urlopen("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/yalova%2C%20turkey?unitGroup=metric&include=days%2Chours&key=secret_value&contentType=json")
+  weather_results_json = urllib.request.urlopen("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/yalova%2C%20turkey?unitGroup=metric&include=days%2Chours&key=5MKDL5XX9XK4RGDWC4B5MJQXE&contentType=json")
 
   # Parse the results as JSON
-  forecasted_weather_data = json.load(weather_results_bytes)
+  forecasted_weather_data = json.load(weather_results_json)
   print(f"jsonData:::{json.dumps(forecasted_weather_data, indent=3)}")
 
 except urllib.error.HTTPError  as e:
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Databricks Wind Turbine Load Prediction Solution Accelerator 
+# Databricks Solution Accelerator Wind Turbine Load Prediction
 git repository: dbsa_wind_turbine_load_prediction
 
 More information about this Databricks Solution Accelerator can be found here: https://www.databricks.com/solutions/accelerators/[INSERT]
